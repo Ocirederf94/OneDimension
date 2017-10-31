a91dd569d5b38ab5e1ad887e3323378dbaf2c22c
Ocirederf94 <frederico.alexandre94@hotmail.com>
2017-10-31 23:01:46 +0100

    Pushing base Project

---
+++ android/src/com/onedimensiongame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.onedimensiongame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.onedimensiongame.OneDimensionGameMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new OneDimensionGameMain(), config);
+	}
+}

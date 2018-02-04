4ac84db22f0b68f2f7596da4c6586760021e8126
Ocirederf94 <frederico.alexandre94@hotmail.com>
2018-02-04 18:19:39 +0100

    Better persistence then before

--- android/src/com/onedimensiongame/MainActivity.java
+++ android/src/com/onedimensiongame/MainActivity.java
@@ -1,25 +1,31 @@
 package com.onedimensiongame;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.android.AndroidApplication;
 
-import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends Activity {
+    private static final String CURRENT_LEVEL = "current_level";
 
     private Button resumeButton;
+    private static Map<String, Integer> dataContainer = new HashMap<>(1);
+    private SaveStateService saveStateService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        saveStateService = SaveStateService.getInstance();
+        dataContainer.put(CURRENT_LEVEL, saveStateService.getLevel(this.getBaseContext()));
         resumeButton = findViewById(R.id.buttonContinue);
         setResumeButton();
     }
@@ -27,55 +33,53 @@ public class MainActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+        saveLevel();
         setResumeButton();
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
+    protected void onStop() {
+        super.onStop();
+        saveLevel();
     }
 
     public void handleStartButton(View view) {
-        handleButton(view);
+        dataContainer.put(CURRENT_LEVEL, 0);
+        handleButton();
     }
 
     public void handleResumeButton(View view) {
-        handleButton(view);
+        handleButton();
     }
 
     public void handleExitButton(View view) {
         finish();
     }
 
-    private void handleButton(View view){
+    private void handleButton() {
         Intent intent = new Intent(this, AndroidLauncher.class);
         intent.setAction(Intent.ACTION_VIEW);
         this.startActivity(intent);
     }
 
-    private void setResumeButton(){
-        resumeButton.setEnabled(false);
-        if (Gdx.app != null) {
-            if (!Gdx.app.getPreferences("continueLevel").get().isEmpty())
-                resumeButton.setEnabled(true);
-        }
+    private void setResumeButton() {
+        resumeButton.setEnabled(dataContainer.get(CURRENT_LEVEL) > 0);
     }
 
+    private void saveLevel() {
+        saveStateService.setLevel(dataContainer.get(CURRENT_LEVEL), this.getBaseContext());
+    }
 
-    private int recoverLvl() throws Exception{
-        try {
-            OutputStreamWriter fout= new OutputStreamWriter(openFileOutput("mi_fichero.txt", Context.MODE_PRIVATE));
-            fout.write("Esto es una prueba!");
-            fout.close();
+    public static class AndroidLauncher extends AndroidApplication {
+        @Override
+        protected void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            try {
+                initialize(new OneDimensionGameMain(dataContainer));
+            } catch (Exception e) {
+                Log.e("Nah", "MainActivity: Error in Initializing OneDimensionGameMain ");
+            }
         }
-        catch (Exception ex)
-        {
-            Log.e("Ficheros", "Error al escribir fichero a memoria interna");
-            return 1;
-        }
-
-        return 0;
-
     }
 
 }
--- android/src/com/onedimensiongame/SaveStateService.java
+++ android/src/com/onedimensiongame/SaveStateService.java
@@ -13,41 +13,41 @@ import java.io.OutputStreamWriter;
 
 public final class SaveStateService {
 
-    private static SaveStateService serviceInstance= new SaveStateService();
+    private static SaveStateService serviceInstance = new SaveStateService();
 
-    private SaveStateService(){}
+    private SaveStateService() {
+    }
 
-    public static SaveStateService getInstance(){
+    public static SaveStateService getInstance() {
         return serviceInstance;
     }
-    public Integer getLevel(Context context){
+
+    public Integer getLevel(Context context) {
         return serviceInstance.recoverLvl(context);
     }
-    public void  setLevel(int lvl, Context context){
-        serviceInstance.saveLvl(lvl, context);
+
+    public void setLevel(int level, Context context) {
+        serviceInstance.saveLvl(level, context);
     }
-    private void saveLvl(int lvl, Context context){
+
+    private void saveLvl(int lvl, Context context) {
         try {
-            OutputStreamWriter fout= new OutputStreamWriter(context.openFileOutput("mi_fichero.txt", Context.MODE_PRIVATE));
-            fout.write(lvl);
-            fout.close();
-        }
-        catch (Exception ex)
-        {
-            Log.e("Ficheros", "Error al escribir fichero a memoria interna");
+            OutputStreamWriter writer = new OutputStreamWriter(context.openFileOutput("levels_file.txt", Context.MODE_PRIVATE));
+            writer.write(lvl);
+            writer.close();
+        } catch (Exception ex) {
+            Log.e("Files", "SaveStateService : Error while writing to memory");
         }
     }
 
-    private Integer recoverLvl(Context context){
-        try{
-            BufferedReader fin = new BufferedReader(new InputStreamReader(context.openFileInput("mi_fichero.txt")));
-            String texto = fin.readLine();
-            fin.close();
-            return  new Integer((texto==null || texto==""? "0":texto));
-        }
-        catch (Exception ex)
-        {
-            Log.e("Ficheros", "Error al leer fichero desde memoria interna");
+    private Integer recoverLvl(Context context) {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(context.openFileInput("levels_file.txt")));
+            String levelString = reader.readLine();
+            reader.close();
+            return levelString == null ? 0 : Integer.valueOf(levelString);
+        } catch (Exception ex) {
+            Log.e("Files", "SaveStateService : Error while reading from memory");
             return 0;
         }
     }

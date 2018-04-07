601460b2813734332b5640c1774149e6e6cdd83b
Ocirederf94 <frederico.alexandre94@hotmail.com>
2018-04-07 03:09:17 +0200

    Adding Music

--- android/src/com/onedimensiongame/MainActivity.java
+++ android/src/com/onedimensiongame/MainActivity.java
@@ -3,6 +3,8 @@ package com.onedimensiongame;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -11,6 +13,7 @@ import android.widget.Button;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,14 +24,20 @@ public class MainActivity extends Activity {
     private static Map<String, Integer> dataContainer = new HashMap<>(1);
     private SaveStateService saveStateService;
     private Context context;
+    private MediaPlayer mainMenuMusic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         context = this.getBaseContext();
+
+        mainMenuMusic = setUpMainMenuMusic();
+        mainMenuMusic.start();
+
         saveStateService = SaveStateService.getInstance(context);
         dataContainer.put(CURRENT_LEVEL, saveStateService.getLevel(context));
+
         resumeButton = findViewById(R.id.buttonContinue);
         setResumeButton();
     }
@@ -36,6 +45,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+        mainMenuMusic.start();
         saveLevel();
         setResumeButton();
     }
@@ -43,12 +53,14 @@ public class MainActivity extends Activity {
     @Override
     protected void onStop() {
         super.onStop();
+        mainMenuMusic.pause();
         saveLevel();
     }
 
     @Override
     protected void onDestroy() {
         saveLevel();
+        mainMenuMusic.pause();
         super.onDestroy();
     }
 
@@ -77,6 +89,21 @@ public class MainActivity extends Activity {
 
     private void saveLevel() {
         saveStateService.setLevel(String.valueOf(dataContainer.get(CURRENT_LEVEL)), context);
+    }
+
+    private MediaPlayer setUpMainMenuMusic() {
+
+        try {
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            AssetFileDescriptor fileDescriptor = getAssets().openFd("audio/main_menu_music.mp3");
+            mediaPlayer.setDataSource(fileDescriptor.getFileDescriptor(), fileDescriptor.getStartOffset(), fileDescriptor.getLength());
+            mediaPlayer.prepare();
+            mediaPlayer.setLooping(true);
+            return mediaPlayer;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public static class AndroidLauncher extends AndroidApplication {
